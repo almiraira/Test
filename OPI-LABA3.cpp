fb3f693d95c0223bb3b9592bd93c2151cbd101fd
almiraira <almiraira@mail.ru>
2023-04-24 17:17:38 +0300

    Test

---
+++ OPI-LABA3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Math.h>
+
+using namespace std;
+double sumOfSeries(int, int);
+int factorial(int);
+
+int main() {
+	int N = 0; double x = 0;
+	cout << "Enter the N: " << endl;
+	cin >> N;
+	cout << "Enter the value x:" << endl;
+	cin >> x;
+	cout << sumOfSeries(N, x);
+	return 0;
+}
+
+double sumOfSeries(int N, int x) {
+	int sum = 0;
+	for (int i = 1; i <= N; i++) {
+		sum += (pow(-1,i-1) * pow(x,2*i-1)) / factorial(2*i-1);
+	}
+	return sum;
+}
+
+
+int factorial(int N) {
+	int fact = 1;
+	for (int i = 1; i <= N; i++) {
+		fact *= i;
+	}
+	return fact;
+}
